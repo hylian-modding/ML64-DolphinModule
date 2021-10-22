92f71591618af5c21745611084ce41ca5c207f93
Denoflions <denvstheworld@gmail.com>
2021-10-22 16:06:36 -0400

    Update EmptyConsole.ts

--- src/ML64-DolphinModule/EmptyConsole.ts
+++ src/ML64-DolphinModule/EmptyConsole.ts
@@ -117,7 +117,7 @@ export class EmptyConsole implements IConsole {
     }
 
     getInternalPlugin(): string {
-        return {} as any;
+        return "";
     }
 
     getRomAccess(): IRomMemory {
